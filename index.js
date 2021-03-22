343a77b8a9164e60cd387c2682887190de20d207
Sakib12345 <sakibalislam12345@gmail.com>
2021-03-23 05:52:50 +0600

    my first node project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+
+const app = express()
+
+
+app.get('/', (req, res) =>{
+    res.send('I Know how to open node')
+})
+
+
+app.listen(3000, () => console.log('listening to port 3000'))
